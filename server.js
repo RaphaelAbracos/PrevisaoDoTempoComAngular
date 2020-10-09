d0b034351c7a0b416e7a8650208b88d94f623cd6
RaphaelAbracos <RaphaelAbracos@github.com>
2020-10-08 23:47:18 -0300

    Alteracoes no server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/Requisicoes'));
 
 app.get('/*', (req, res)=> {
-  res.sendFile(__dirname + '/dist/angular-teste/index.html');
+  res.sendFile(__dirname + '/dist/Requisicoes/index.html');
 })
 
 app.listen(PORT, ()=>{
